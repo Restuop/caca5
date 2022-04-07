b651023682d6e7d85c67b302898c1b6df65f2087
RESTU1L <93248075+RESTU1L@users.noreply.github.com>
2022-04-08 03:59:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Zifabotz
 
 let fs = require('fs')
-global.owner = ['6285828764046', '6289696163176','6285828764046', '6285828764046', '6285828764046'] // 6289696163176
+global.owner = ['6289696163176', '6289696163176','6289696163176', '6289696163176', '6289696163176'] // 6289696163176
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -45,7 +45,7 @@ global.packname = 'rozi'
 global.author = 'zifa'
 
 global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
-global.eror = '「❗」Server LAGI ERROR COK!'
+global.eror = '「❗」Server LAGI ERROR !'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© zifabotz' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
