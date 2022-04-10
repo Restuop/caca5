349bc97553ccb5f75f9dce3764931c0884108836
Restuop <103339662+Restuop@users.noreply.github.com>
2022-04-10 21:17:16 +0700

    Update zifabotz.js

--- plugins/zifabotz.js
+++ plugins/zifabotz.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let caption = `*APA OI!!, MANGGIL” GAK JELAS*`
+let caption = `*APA BANG MANGGIL²*`
 
 conn.sendButton( m.chat, caption, `©️ zifabotz`, `Menu`, `.?`, m)
 
